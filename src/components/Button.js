f988d3c6b0888ec06e77b868648b3db5e8977408
topeojewale <topeapps@gmail.com>
2021-07-30 01:12:02 +0100

    added components and styles

---
+++ src/components/Button.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Button.scss'
+
+const Button = () => {
+    return (
+        <button className="save-btn">Create Account</button>
+    )
+}
+
+export default Button
